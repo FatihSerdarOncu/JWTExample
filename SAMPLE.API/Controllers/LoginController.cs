e2815038d2f6e9d19c8332723ac867a6edfc1e1b
FatihSerdarOncu <33923374+FatihSerdarOncu@users.noreply.github.com>
2021-01-19 00:43:56 +0300

    init

---
+++ SAMPLE.API/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SAMPLE.API.Model;
+
+namespace SAMPLE.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginController : ControllerBase
+	{
+        private readonly AppSettings _appSettings;
+
+        public LoginController(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+        [HttpGet("login")]
+        public VerifyModel Login(string username,string password)
+        {
+            //var user = _users.SingleOrDefault(x => x.Username == model.Username && x.Password == model.Password); => user karşılaştırması
+            var user = new User() { Id = 1, Name = "serdar" };
+
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var token = generateJwtToken(user);
+
+            return new VerifyModel(true, token);
+        }
+
+
+        private string generateJwtToken(User user)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ SAMPLE.API/Model/VerifyModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAMPLE.API.Model
+{
+	public class VerifyModel
+	{
+		public VerifyModel(bool Verify, string Token)
+		{
+			this.Verify = Verify;
+			this.Token = Token;
+		}
+		public bool Verify { get; set; }
+		public string Token { get; set; }
+	}
+}
